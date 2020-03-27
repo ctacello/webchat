b371ca09e02264e7aa3c32732aabe1ca2087d020
ctacello <ctacello@yandex.ru>
2020-03-27 23:43:20 +0200

    auth with google  sign-in, sign-out, register

---
+++ public/dist/main.js
@@ -0,0 +1,250 @@
+// alert('hello!');
+class App {
+    constructor () {
+        this.currentUser = null;
+        this.db = firebase.firestore();
+
+        this.initFirebaseListeners();
+        this.messages = [];
+    }
+
+    initFirebaseListeners() {
+        this.db.collection('messages').onSnapshot((snapshot) => {
+            snapshot.forEach(doc => {
+                // console.log(doc);
+
+                let message = doc.data();
+                message.id = doc.id;
+
+                if (!this.messages.find(m => m.id == message.id)) {
+                    this.messages.push(message);
+                    this.displayMessage(message);
+                }
+                // console.log('messages');
+            })
+        })
+    }
+
+    displayMessage(message) {
+        let messageStorage = document.querySelector('.main__messages');
+
+            let divMessage = document.createElement('div');
+            divMessage.classList = 'main__message';
+
+                let h4 = document.createElement('h4');
+                h4.innerText = message.username;
+                h4.className = 'main__nickname';
+
+                let span = document.createElement('span');
+                span.innerText = message.message;
+                span.className = 'main__text';
+
+                divMessage.appendChild(h4);
+                divMessage.appendChild(span);
+        
+        messageStorage.appendChild(divMessage);
+    }
+
+    displayUser() {
+        popupWindow();
+
+        let signOutButton = document.querySelector('.header__signout');
+        signOutButton.hidden = false;
+
+        let userName = document.querySelector('.header__username');
+        userName.hidden = false;
+        userName.innerText = this.currentUser.displayName;
+
+        let userPic = document.querySelector('.header__userpic');
+        userPic.hidden = false;
+        userPic.innerHTML = '';
+
+        // let img = document.createElement('img');
+        // img.src = this.currentUser.photoURL;
+        userPic.style.backgroundImage = `url(${this.currentUser.photoURL})`;
+
+        // userPic.appendChild(img);
+
+        signInButton.hidden = true;
+
+        let userList = document.querySelector('.main__users');
+
+            let usernameInUserList = document.createElement('div');
+            usernameInUserList.className = 'main_userlist';
+
+                let a = document.createElement('a');
+                a.className = 'main__userinfo';
+                a.href = 'http://';
+                a.innerText = '[i]';
+
+                let span = document.createElement('span');
+                span.innerText = ' ' + this.currentUser.displayName;
+
+            usernameInUserList.appendChild(a);
+            usernameInUserList.appendChild(span);
+            
+        userList.appendChild(usernameInUserList);
+    }
+
+    signOutUser() {
+        let signOutButton = document.querySelector('.header__signout');
+        signOutButton.hidden = true;
+
+        let userName = document.querySelector('.header__username');
+        userName.hidden = true;
+
+        let userPic = document.querySelector('.header__userpic');
+        userPic.hidden = true;
+
+        signInButton.hidden = false;
+    }
+
+    signInWithGoogle = async () => {
+        let provider = new firebase.auth.GoogleAuthProvider();
+        let resultAuth = await firebase.auth().signInWithPopup(provider);
+        // console.log(resultAuth);
+        this.currentUser = resultAuth.user;
+
+        this.displayUser();
+    }
+
+    registerUser = () => {
+
+        let email = document.querySelector('.main__email').value;
+        let password = document.querySelector('.main__password').value;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            console.log('Create user errorCode: ', errorCode);
+            var errorMessage = error.message;
+            console.log('Create user errorMessage: ', errorMessage);
+            // ...
+          });
+
+        this.displayUser();
+    }
+
+    signInWithEmailAndPassword = async () => {
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            console.log('Login errorCode: ', errorCode);
+            var errorMessage = error.message;
+            console.log('Login errorMessage: ', errorMessage);
+            // ...
+          });
+    }
+
+    signOut = () => {
+        firebase.auth().signOut();
+        this.signOutUser();
+    }
+
+    // попытка сделать сессию сохраняемой
+
+    // signIn = () => {
+    //     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    //     .then(async () => {
+    //         let provider = new firebase.auth.GoogleAuthProvider();
+    //         let resultAuth = await firebase.auth().signInWithPopup(provider);
+
+    //         this.currentUser = resultAuth.user;
+            
+    //         console.log(this);
+    //         console.log(this.currentUser);
+
+    //         this.displayUser();
+
+    //     return resultAuth;
+    //     })
+    //     .catch(function(error) {
+    //         // Handle Errors here.
+    //         let errorCode = error.code;
+    //         let errorMessage = error.message;
+    //         console.log(errorCode);
+    //         console.log(errorMessage);
+    //     });
+       
+    // }
+
+    textMessage = (e) => {
+        console.log(e);
+        if (e.target.value) {
+            formButton.disabled = false;
+        } else {
+            formButton.disabled = true;
+        }  
+    }
+    sendMessage = (e) => {
+        e.preventDefault();
+
+        let messageBody = e.target[0].value;
+
+        this.db.collection('messages').add({
+            username: this.currentUser.displayName,
+            message: messageBody
+        })
+    }
+}
+
+let myChat = new App();
+
+// register Email
+// let email = document.querySelector('.main__email');
+// email.addEventListener('submit', myChat.registerEmail);
+
+// let password = document.querySelector('.main__password');
+// password.addEventListener('submit', myChat.registerPassword)
+
+// register User Email and Password
+let registerLink = document.querySelector('.main__register');
+registerLink.addEventListener('click', myChat.registerUser);
+
+// auth Email and Password
+let logInButton = document.querySelector('.main__signinbutton');
+logInButton.addEventListener('click', myChat.signInWithEmailAndPassword);
+
+// auth Google
+let logInButtonGoogle  = document.querySelector('.main__button-google');
+logInButtonGoogle.addEventListener('click', myChat.signInWithGoogle);
+
+// signout
+let signOutButton = document.querySelector('.header__signout');
+signOutButton.addEventListener('click', myChat.signOut);
+
+// chat msg and enable send_button
+let message = document.querySelector('.main__input');
+let formButton = document.querySelector('.main__send');
+message.addEventListener('input', myChat.textMessage);
+
+// send msg
+let form = document.querySelector('.main__input-form');
+form.addEventListener('submit', myChat.sendMessage);
+
+let currentUsersOnline = () => {
+
+    let usersOnline = document.querySelector('.main__usersonline');
+
+        let number = document.querySelectorAll('.main__userslist').length;
+
+    usersOnline.innerHTML = 'Current users online: ' + number;
+}
+
+currentUsersOnline();
+
+// popup window
+
+let popupWindow = function(){
+    document.querySelector('.main__popup').classList.toggle('main__popup_display-none');
+    document.querySelector('.main__popup').classList.toggle('main__popup_display-flex');
+}
+
+// display popup window
+let signInButton = document.querySelector('.header__signin');
+signInButton.hidden = false;
+signInButton.addEventListener('click', popupWindow);
+
+// hide popup window
+let closePopupWindow = document.querySelector('.main__close');
+closePopupWindow.addEventListener('click', popupWindow);
